cd826f74cb2b0e06d8fb3950d49dd933f0800d28
Denver009 <ghostelectrified@gmail.com>
2025-07-29 00:09:14 +0530

    Making changes to the searchAnimeandetails.py file to return the list of anime details instead of printing them directly. This allows for better handling of the data in other parts of the application.

--- providers/Hianime/Scraper/searchAnimeandetails.py
+++ providers/Hianime/Scraper/searchAnimeandetails.py
@@ -13,34 +13,66 @@ def searchAnimeandetails(name):
     url = configure['baseurl']
     html = requests.get(f'{url}/search?keyword={name}'.format(search=name), auth=('user', 'pass'))
     soup = BeautifulSoup(html.text, 'html.parser')
-    junk = soup.find_all(class_='flw-item')
-    for recycled_junk in junk:
-        anime_name = recycled_junk.find('h3', class_='film-name').find('a')['title']
-        anime_jname = recycled_junk.find('h3', class_='film-name').find('a').get('data-jname', '')
-        type_span = recycled_junk.find('span', class_='fdi-item')
-        type = type_span.text.strip() if type_span else ''
-        duration_span = recycled_junk.find('span', class_='fdi-duration')
-        duration = duration_span.text.strip() if duration_span else ''
-        cc_div = recycled_junk.find('div', class_='tick-item tick-sub')
-        closed_caption_count = cc_div.text.strip() if cc_div else ''
-        img = recycled_junk.find('img', class_='film-poster-img')
-        image_url = img['data-src'] if img else ''
-        watch_link = ''
-        watch_a = recycled_junk.find('a', class_='film-poster-ahref')
-        if watch_a:
-            watch_link = watch_a['href']
-        No += 1
+    anime_blocks = soup.find_all('div', class_='flw-item')
+    for block in anime_blocks:
 
-        # Appending the details to the list
-        list_of_anime.append({
-            'Sl-no' : No,
-            'Name' : anime_name,
-            'Japanese Name' : anime_jname,
-            'Type' : type,
-            'Duration' : duration,
-            'Closed Caption Count' : closed_caption_count,
-            'Image URL' : image_url,
-            'URL' : watch_link,
-        })
-        return list_of_anime
+        # Extract basic info
+        title_tag = block.find('h3', class_='film-name').find('a')
+        title = title_tag.get('title', '').strip()
+        japanese_name = title_tag.get('data-jname', '').strip()
+
+        # Type and Duration - span elements in fd-infor div
+        fd_infor = block.find('div', class_='fd-infor')
+        if fd_infor:
+            type_span = fd_infor.find('span', class_='fdi-item')
+            type_ = type_span.text.strip() if type_span else 'N/A'
+
+            duration_span = fd_infor.find('span', class_='fdi-duration')
+            duration = duration_span.text.strip() if duration_span else 'N/A'
+        else:
+            type_ = 'N/A'
+            duration = 'N/A'
+
+        # Image URL
+        img_tag = block.find('img', class_='film-poster-img')
+        image_url = img_tag.get('data-src', '').strip() if img_tag else 'N/A'
+
+        # Watch Link
+        watch_a = block.find('a', class_='film-poster-ahref')
+        watch_link = watch_a.get('href', '').strip() if watch_a else 'N/A'
+
+        # Episode/Sub/Dub info in div.tick-item inside div.tick
+        tick_div = block.find('div', class_='tick ltr')
+        if tick_div:
+            sub_div = tick_div.find('div', class_='tick-item tick-sub')
+            sub_episodes = sub_div.text.strip() if sub_div and sub_div.text.strip() else 'N/A'
+
+            dub_div = tick_div.find('div', class_='tick-item tick-dub')
+            dub_episodes = dub_div.text.strip() if dub_div and dub_div.text.strip() else 'N/A'
+
+            eps_div = tick_div.find('div', class_='tick-item tick-eps')
+            total_episodes = eps_div.text.strip() if eps_div and eps_div.text.strip() else 'N/A'
+        else:
+            sub_episodes = 'N/A'
+            dub_episodes = 'N/A'
+            total_episodes = 'N/A'
+
+        anime_details = {
+            'No': No + 1,
+            'Title': title,
+            'Japanese Name': japanese_name,
+            'Type': type_,
+            'Duration': duration,
+            'Sub Episodes Available': sub_episodes,
+            'Dub Episodes Available': dub_episodes,
+            'Total Episodes': total_episodes,
+            'Image URL': image_url,
+            'Watch Link': watch_link
+        }
+
+        list_of_anime.append(anime_details)
+    return list_of_anime
+    
+    
+print(searchAnimeandetails("Naruto"))  # Example usage
 
